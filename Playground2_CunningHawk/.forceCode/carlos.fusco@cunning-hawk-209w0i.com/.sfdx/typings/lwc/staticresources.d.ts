180fae107b5f6a9fe13176ea541fb13c46925381
carlosfusco <carlos_fusco@hotmail.com>
2020-03-28 19:20:27 +0000

    Playground2.

---
+++ Playground2_CunningHawk/.forceCode/carlos.fusco@cunning-hawk-209w0i.com/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,40 @@
+declare module "@salesforce/resourceUrl/AgentAvatar" {
+    var AgentAvatar: string;
+    export default AgentAvatar;
+}
+declare module "@salesforce/resourceUrl/DataCache" {
+    var DataCache: string;
+    export default DataCache;
+}
+declare module "@salesforce/resourceUrl/Logo" {
+    var Logo: string;
+    export default Logo;
+}
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/countup7" {
+    var countup7: string;
+    export default countup7;
+}
+declare module "@salesforce/resourceUrl/heatmap" {
+    var heatmap: string;
+    export default heatmap;
+}
+declare module "@salesforce/resourceUrl/heatmapmock" {
+    var heatmapmock: string;
+    export default heatmapmock;
+}
+declare module "@salesforce/resourceUrl/leaflet" {
+    var leaflet: string;
+    export default leaflet;
+}
+declare module "@salesforce/resourceUrl/leaflet1" {
+    var leaflet1: string;
+    export default leaflet1;
+}
+declare module "@salesforce/resourceUrl/nouislider" {
+    var nouislider: string;
+    export default nouislider;
+}
